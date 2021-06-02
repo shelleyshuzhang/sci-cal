f577438529cce9e2d96898d35cc44a9003cd7b50
Shu <zhang.shu4@husky.neu.edu>
2021-06-03 05:27:33 +0800

    Improve Task 1 - RealNumOps and BigNumOps Code Style

--- RealNumOps.c
+++ RealNumOps.c
@@ -1,474 +1,440 @@
-#include <stdlib.h>
-#include <string.h>
 #include "RealNumOps.h"
-#include "BigNumOps.h"
 
-#define ACCURITY 10
+#define ACCURACY 10
 
-int point_locate(char *a)
-{
-	int len = strlen(a);
-	int i;
-	for (i = len -1; i >=0; i--) {
-		if (a[i] == '.') {
-			return len - 1 - i;
-		}
-	}
-	return 0;
+int point_locate(char *a) {
+    int len = strlen(a);
+    int i;
+    for (i = len - 1; i >= 0; i--) {
+        if (a[i] == '.') {
+            return len - 1 - i;
+        }
+    }
+    return 0;
 }
 
-char *fill_zero(char *a, int osize ,int nsize)
-{
-	char *n = malloc((nsize + 1));
-	int i, j = 0;
-	for (i = 0; i < osize; i++) {
-		if ((a[i] != '-') && a[i] != '+' && a[i] != '.' && a[i] != '0') {
-			break;
-		}
-	}
-	if (i == osize) {
-		n[j] = '0';
-		j++;
-		goto done;
-	}
-	for (; i < osize; i++) {
-		if ((a[i] != '-') && a[i] != '+' && a[i] != '.') {
-			n[j] = a[i];
-			j++;
-		}
-	}
-	for (i = 0; i < nsize - osize; i++) {
-		n[j] = '0';
-		j++;
-	}
-done:
-	n[j] = 0;
-	return n;
+char *fill_zero(char *a, int osize, int nsize) {
+    char *n = malloc((nsize + 1));
+    int i, j = 0;
+    for (i = 0; i < osize; i++) {
+        if ((a[i] != '-') && a[i] != '+' && a[i] != '.' && a[i] != '0') {
+            break;
+        }
+    }
+    if (i == osize) {
+        n[j] = '0';
+        j++;
+        goto done;
+    }
+    for (; i < osize; i++) {
+        if ((a[i] != '-') && a[i] != '+' && a[i] != '.') {
+            n[j] = a[i];
+            j++;
+        }
+    }
+    for (i = 0; i < nsize - osize; i++) {
+        n[j] = '0';
+        j++;
+    }
+    done:
+    n[j] = 0;
+    return n;
 }
 
-char *remove_zero(char *a)
-{
-	int i = 0, j;
-	int zero = 0;
-	int len = strlen(a);
+char *remove_zero(char *a) {
+    int i = 0, j;
+    int zero = 0;
+    int len = strlen(a);
 
-	if (a[0] == '-' || a[0] == '+')
-		i++;
-	for (; i < len; i++) {
-		if (a[i] != '0') {
-			break;
-		}
-		zero++;
-	}
-	if (i == len) {
-		i--;
-		zero--;
-	}
-	if (a[i] == '.') {
-		i--;
-		zero--;
-	}
-	for (j = i - zero; j + zero < len; j++) {
-		a[j] = a[j + zero];
-	}
-	a[j] = 0;
-	return a;
+    if (a[0] == '-' || a[0] == '+')
+        i++;
+    for (; i < len; i++) {
+        if (a[i] != '0') {
+            break;
+        }
+        zero++;
+    }
+    if (i == len) {
+        i--;
+        zero--;
+    }
+    if (a[i] == '.') {
+        i--;
+        zero--;
+    }
+    for (j = i - zero; j + zero < len; j++) {
+        a[j] = a[j + zero];
+    }
+    a[j] = 0;
+    return a;
 }
 
-char *format_real(char *a, int sign, int point)
-{
-	int i, j = 0;
-	int len = strlen(a);
-	int size = len >= point ? len : point;
-	char *c;
-	if (sign)
-		size += 1;
-	if (point)
-		size += 1;
+char *format_real(char *a, int sign, int point) {
+    int i, j = 0;
+    int len = strlen(a);
+    int size = len >= point ? len : point;
+    char *c;
+    if (sign)
+        size += 1;
+    if (point)
+        size += 1;
 
-	c = malloc((size + 3) * sizeof(char));
+    c = malloc((size + 3) * sizeof(char));
 
-	if (sign) {
-		c[0] = '-';
-		j++;
-	}
+    if (sign) {
+        c[0] = '-';
+        j++;
+    }
 
-	if (point >= len) {
-		c[j] = '0';
-		j++;
-		c[j] = '.';
-		j++;
-		for (i = 0; i < point - len; i++) {
-			c[j] = '0';
-			j++;
-		}
-		for (i = 0; i < len; i++) {
-			c[j] = a[i];
-			j++;
-		}
-	} else {
-		for (i = 0; i < len; i++) {
-			if ((point !=0) && ((i + point) == (len))) {
-				c[j] = '.';
-				j++;
-			}
-			c[j] = a[i];
-			j++;
-		}
-	}
-	c[j] = 0;
-	if (point != 0) {
-		for (i = j - 1; i >= 1; i--) {
-			if (c[i] == '0')
-				c[i] = 0;
-			else
-				break;
-		}
-		if (c[i] == '.')
-			c[i] = 0;
-	}
+    if (point >= len) {
+        c[j] = '0';
+        j++;
+        c[j] = '.';
+        j++;
+        for (i = 0; i < point - len; i++) {
+            c[j] = '0';
+            j++;
+        }
+        for (i = 0; i < len; i++) {
+            c[j] = a[i];
+            j++;
+        }
+    } else {
+        for (i = 0; i < len; i++) {
+            if ((point != 0) && ((i + point) == (len))) {
+                c[j] = '.';
+                j++;
+            }
+            c[j] = a[i];
+            j++;
+        }
+    }
+    c[j] = 0;
+    if (point != 0) {
+        for (i = j - 1; i >= 1; i--) {
+            if (c[i] == '0')
+                c[i] = 0;
+            else
+                break;
+        }
+        if (c[i] == '.')
+            c[i] = 0;
+    }
 
-	free(a);
-	return remove_zero(c);
+    free(a);
+    return remove_zero(c);
 }
 
-char *make_prec(char *a, int prec)
-{
-	int i, j;
-	int len = strlen(a);
+char *make_prec(char *a, int prec) {
+    int i;
+    int len = strlen(a);
 
-	for (i = 0 ; i < len; i++) {
-		if (a[i] == '.') {
-			if ((i + prec + 1) < len)
-				a[i + prec + 1] = 0;
-		}
-	}
-	return a;
+    for (i = 0; i < len; i++) {
+        if (a[i] == '.') {
+            if ((i + prec + 1) < len)
+                a[i + prec + 1] = 0;
+        }
+    }
+    return a;
 }
 
-int RealBigNumCmp(char *a, char *b)
-{
-	int i;
-	char *point_a, *point_b;
-	char *c;
-	int lena = strlen(a);
-	int lenb = strlen(b);
-	int judge;
-	
-	int apoint = point_locate(a);
-	int bpoint = point_locate(b);
-	int mpoint = apoint > bpoint ? apoint : bpoint;
+int RealBigNumCmp(char *a, char *b) {
+    char *point_a, *point_b;
+    int lena = strlen(a);
+    int lenb = strlen(b);
+    int judge;
 
-	int asign = a[0] == '-' ? 1 : 0;
-	int bsign = b[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int bpoint = point_locate(b);
+    int mpoint = apoint > bpoint ? apoint : bpoint;
 
-	if (asign ^ bsign) {
-		if (bsign)
-			return 1;
-		if (asign)
-			return -1;
-	}
+    int asign = a[0] == '-' ? 1 : 0;
+    int bsign = b[0] == '-' ? 1 : 0;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint);
-	point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
+    if (asign ^ bsign) {
+        if (bsign)
+            return 1;
+        if (asign)
+            return -1;
+    }
 
-	judge = (asign > 0 ? -1 : 1) * UnsignedBigNumCmp(point_a, point_b);
+    point_a = fill_zero(a, lena, lena + mpoint - apoint);
+    point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
 
-	free(point_a);
-	free(point_b);
-	return judge;
+    judge = (asign > 0 ? -1 : 1) * UnsignedBigNumCmp(point_a, point_b);
+
+    free(point_a);
+    free(point_b);
+    return judge;
 }
 
-int RealBigNumAbsCmp(char *a, char *b)
-{
-	int i;
-	char *point_a, *point_b;
-	char *c;
-	int lena = strlen(a);
-	int lenb = strlen(b);
-	int judge;
-	
-	int apoint = point_locate(a);
-	int bpoint = point_locate(b);
-	int mpoint = apoint > bpoint ? apoint : bpoint;
+int RealBigNumAbsCmp(char *a, char *b) {
+    char *point_a, *point_b;
+    int lena = strlen(a);
+    int lenb = strlen(b);
+    int judge;
 
-	int asign = a[0] == '-' ? 1 : 0;
-	int bsign = b[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int bpoint = point_locate(b);
+    int mpoint = apoint > bpoint ? apoint : bpoint;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint);
-	point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
+    point_a = fill_zero(a, lena, lena + mpoint - apoint);
+    point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
 
-	judge = UnsignedBigNumCmp(point_a, point_b);
+    judge = UnsignedBigNumCmp(point_a, point_b);
 
-	free(point_a);
-	free(point_b);
-	return judge;
+    free(point_a);
+    free(point_b);
+    return judge;
 }
 
-char *RealBigNumAdd(char *a, char *b)
-{
-	int i;
-	char *point_a, *point_b;
-	char *c;
-	int lena = strlen(a);
-	int lenb = strlen(b);
-	
-	int apoint = point_locate(a);
-	int bpoint = point_locate(b);
-	int mpoint = apoint > bpoint ? apoint : bpoint;
+char *RealBigNumAdd(char *a, char *b) {
+    char *point_a, *point_b;
+    char *c;
+    int lena = strlen(a);
+    int lenb = strlen(b);
 
-	int asign = a[0] == '-' ? 1 : 0;
-	int bsign = b[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int bpoint = point_locate(b);
+    int mpoint = apoint > bpoint ? apoint : bpoint;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint);
-	point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
+    int asign = a[0] == '-' ? 1 : 0;
+    int bsign = b[0] == '-' ? 1 : 0;
 
-	if (asign ^ bsign) {
-		if (bsign)
-			c = UnsignedBigNumSub(point_a, point_b);
-		if (asign)
-			c = UnsignedBigNumSub(point_b, point_a);
-		if (c[0] == '-') {
-			asign = 1;
-			c[0] = '0';
-		} else
-			asign = 0;
-	} else {
-		c = UnsignedBigNumAdd(point_a, point_b);	
-	}
+    point_a = fill_zero(a, lena, lena + mpoint - apoint);
+    point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
 
-	free(point_a);
-	free(point_b);
+    if (asign ^ bsign) {
+        if (bsign)
+            c = UnsignedBigNumSub(point_a, point_b);
+        if (asign)
+            c = UnsignedBigNumSub(point_b, point_a);
+        if (c[0] == '-') {
+            asign = 1;
+            c[0] = '0';
+        } else
+            asign = 0;
+    } else {
+        c = UnsignedBigNumAdd(point_a, point_b);
+    }
 
-	return format_real(c, asign, mpoint);
+    free(point_a);
+    free(point_b);
+
+    return format_real(c, asign, mpoint);
 }
 
-char *RealBigNumSub(char *a, char *b)
-{
-	int i;
-	char *point_a, *point_b;
-	char *c;
-	int lena = strlen(a);
-	int lenb = strlen(b);
-	
-	int apoint = point_locate(a);
-	int bpoint = point_locate(b);
-	int mpoint = apoint > bpoint ? apoint : bpoint;
+char *RealBigNumSub(char *a, char *b) {
+    char *point_a, *point_b;
+    char *c;
+    int lena = strlen(a);
+    int lenb = strlen(b);
 
-	int asign = a[0] == '-' ? 1 : 0;
-	int bsign = b[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int bpoint = point_locate(b);
+    int mpoint = apoint > bpoint ? apoint : bpoint;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint);
-	point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
+    int asign = a[0] == '-' ? 1 : 0;
+    int bsign = b[0] == '-' ? 1 : 0;
 
-	if (asign ^ bsign) {
-		c = UnsignedBigNumAdd(point_a, point_b);
-	} else {
-		c = UnsignedBigNumSub(point_a, point_b);	
-		if (c[0] == '-') {
-			asign ^= 1;
-			c[0] = '0';
-		}
-	}
+    point_a = fill_zero(a, lena, lena + mpoint - apoint);
+    point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
 
-	free(point_a);
-	free(point_b);
+    if (asign ^ bsign) {
+        c = UnsignedBigNumAdd(point_a, point_b);
+    } else {
+        c = UnsignedBigNumSub(point_a, point_b);
+        if (c[0] == '-') {
+            asign ^= 1;
+            c[0] = '0';
+        }
+    }
 
-	return format_real(c, asign, mpoint);
-	return NULL;
+    free(point_a);
+    free(point_b);
+
+    return format_real(c, asign, mpoint);
 }
 
-char *RealBigNumMul(char *a, char *b)
-{
-	int i;
-	char *point_a, *point_b;
-	char *c;
-	int lena = strlen(a);
-	int lenb = strlen(b);
-	
-	int apoint = point_locate(a);
-	int bpoint = point_locate(b);
-	int mpoint = apoint > bpoint ? apoint : bpoint;
+char *RealBigNumMul(char *a, char *b) {
+    char *point_a, *point_b;
+    char *c;
+    int lena = strlen(a);
+    int lenb = strlen(b);
 
-	int asign = a[0] == '-' ? 1 : 0;
-	int bsign = b[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int bpoint = point_locate(b);
+    int mpoint = apoint > bpoint ? apoint : bpoint;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint);
-	point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
+    int asign = a[0] == '-' ? 1 : 0;
+    int bsign = b[0] == '-' ? 1 : 0;
 
-	if (asign ^ bsign) {
-		asign = 1;
-	} else {
-		asign = 0;
-	}
-	c = UnsignedBigNumMul(point_a, point_b);
+    point_a = fill_zero(a, lena, lena + mpoint - apoint);
+    point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
 
-	free(point_a);
-	free(point_b);
+    if (asign ^ bsign) {
+        asign = 1;
+    } else {
+        asign = 0;
+    }
+    c = UnsignedBigNumMul(point_a, point_b);
 
-	return format_real(c, asign, mpoint + mpoint);
+    free(point_a);
+    free(point_b);
+
+    return format_real(c, asign, mpoint + mpoint);
 }
 
-char *RealBigNumDiv(char *a, char *b)
-{
-	int i;
-	char *point_a, *point_b;
-	char *c;
-	int lena = strlen(a);
-	int lenb = strlen(b);
-	
-	int apoint = point_locate(a);
-	int bpoint = point_locate(b);
-	int mpoint = apoint > bpoint ? apoint : bpoint;
+char *RealBigNumDiv(char *a, char *b) {
+    char *point_a, *point_b;
+    char *c;
+    int lena = strlen(a);
+    int lenb = strlen(b);
+
+    int apoint = point_locate(a);
+    int bpoint = point_locate(b);
+    int mpoint = apoint > bpoint ? apoint : bpoint;
 
 #if 0
-	int aint = lena - apoint;
-	int bint = lenb - bpoint;
-	int mint = aint > bint ? aint : bint;
+    int aint = lena - apoint;
+    int bint = lenb - bpoint;
+    int mint = aint > bint ? aint : bint;
 #endif
 
-	int asign = a[0] == '-' ? 1 : 0;
-	int bsign = b[0] == '-' ? 1 : 0;
+    int asign = a[0] == '-' ? 1 : 0;
+    int bsign = b[0] == '-' ? 1 : 0;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint + ACCURITY);
-	point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
+    point_a = fill_zero(a, lena, lena + mpoint - apoint + ACCURACY);
+    point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
 
-	if (asign ^ bsign) {
-		asign = 1;
-	} else {
-		asign = 0;
-	}
+    if (asign ^ bsign) {
+        asign = 1;
+    } else {
+        asign = 0;
+    }
 
-	c = UnsignedBigNumDiv(point_a, point_b);
+    c = UnsignedBigNumDiv(point_a, point_b);
 
-	free(point_a);
-	free(point_b);
+    free(point_a);
+    free(point_b);
 
-	if (strcmp(c, "inf") == 0)
-		return c;
+    if (strcmp(c, "inf") == 0)
+        return c;
 
-	return format_real(c, asign, ACCURITY);
+    return format_real(c, asign, ACCURACY);
 }
 
-char *RealBigNumDiv_Rem(char *a, char *b, char *rem)
-{
-	int i;
-	char *point_a, *point_b;
-	char *c, *t;
-	int lena = strlen(a);
-	int lenb = strlen(b);
-	
-	int apoint = point_locate(a);
-	int bpoint = point_locate(b);
-	int mpoint = apoint > bpoint ? apoint : bpoint;
+char *RealBigNumDiv_Rem(char *a, char *b, char *rem) {
+    char *point_a, *point_b;
+    char *c, *t;
+    int lena = strlen(a);
+    int lenb = strlen(b);
+
+    int apoint = point_locate(a);
+    int bpoint = point_locate(b);
+    int mpoint = apoint > bpoint ? apoint : bpoint;
 
 #if 0
-	int aint = lena - apoint;
-	int bint = lenb - bpoint;
-	int mint = aint > bint ? aint : bint;
+    int aint = lena - apoint;
+    int bint = lenb - bpoint;
+    int mint = aint > bint ? aint : bint;
 #endif
 
-	int asign = a[0] == '-' ? 1 : 0;
-	int bsign = b[0] == '-' ? 1 : 0;
+    int asign = a[0] == '-' ? 1 : 0;
+    int bsign = b[0] == '-' ? 1 : 0;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint);
-	point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
+    point_a = fill_zero(a, lena, lena + mpoint - apoint);
+    point_b = fill_zero(b, lenb, lenb + mpoint - bpoint);
 
-	if (asign ^ bsign) {
-		asign = 1;
-	} else {
-		asign = 0;
-	}
+    if (asign ^ bsign) {
+        asign = 1;
+    } else {
+        asign = 0;
+    }
 
-	t = malloc(sizeof(char) * (lenb + 3));
-	c = UnsignedBigNumDiv_Rem(point_a, point_b, t);
+    t = malloc(sizeof(char) * (lenb + 3));
+    c = UnsignedBigNumDiv_Rem(point_a, point_b, t);
 
-	if (rem) {
-		t = format_real(t, 0, mpoint);
-		memcpy(rem, t, strlen(t));
-		rem[strlen(t)] = 0;
-	}
-	free(t);
+    if (rem) {
+        t = format_real(t, 0, mpoint);
+        memcpy(rem, t, strlen(t));
+        rem[strlen(t)] = 0;
+    }
+    free(t);
 
-	free(point_a);
-	free(point_b);
+    free(point_a);
+    free(point_b);
 
-	if (strcmp(c, "inf") == 0)
-		return c;
+    if (strcmp(c, "inf") == 0)
+        return c;
 
-	return format_real(c, asign, 0);
+    return format_real(c, asign, 0);
 }
 
-char *RealBigNumPow(char *a, int num)
-{
-	int i;
-	char *point_a;
-	char *c;
-	int lena = strlen(a);
-	
-	int apoint = point_locate(a);
-	int mpoint = apoint;
+char *RealBigNumPow(char *a, int num) {
+    char *point_a;
+    char *c;
+    int lena = strlen(a);
 
-	int asign = a[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int mpoint = apoint;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint);
+    int asign = a[0] == '-' ? 1 : 0;
 
-	c = UnsignedBigNumPow(point_a, num);
+    point_a = fill_zero(a, lena, lena + mpoint - apoint);
 
-	if ((num % 2) == 0)
-		asign = 0;
+    c = UnsignedBigNumPow(point_a, num);
 
-	free(point_a);
+    if ((num % 2) == 0)
+        asign = 0;
 
-	return format_real(c, asign, mpoint * num);
+    free(point_a);
+
+    return format_real(c, asign, mpoint * num);
 }
 
-char *RealBigNumSqrt(char *a)
-{
-	int i;
-	char *point_a;
-	char *c;
-	int lena = strlen(a);
-	
-	int apoint = point_locate(a);
-	int mpoint = (apoint % 2) + apoint;
+char *RealBigNumSqrt(char *a) {
+    char *point_a;
+    char *c;
+    int lena = strlen(a);
 
-	int asign = a[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int mpoint = (apoint % 2) + apoint;
 
-	if (asign < -1) {
+    int asign = a[0] == '-' ? 1 : 0;
+
+    if (asign < -1) {
         c = malloc(sizeof(char) * 4);
         memcpy(c, "inf", 3);
         c[3] = 0;
-		return c;
+        return c;
     }
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint + ACCURITY * 2 );
+    point_a = fill_zero(a, lena, lena + mpoint - apoint + ACCURACY * 2);
 
-	c = UnsignedBigNumSqrt(point_a);
+    c = UnsignedBigNumSqrt(point_a);
 
-	free(point_a);
+    free(point_a);
 
-	return format_real(c, asign, (mpoint / 2) + ACCURITY);
+    return format_real(c, asign, (mpoint / 2) + ACCURACY);
 }
 
-char *RealBigNumRoot(char *a, int num)
-{
-	int i;
-	char *point_a;
-	char *c;
-	int lena = strlen(a);
-	
-	int apoint = point_locate(a);
-	int mpoint = (num - (apoint % num)) + apoint;
+char *RealBigNumRoot(char *a, int num) {
+    char *point_a;
+    char *c;
+    int lena = strlen(a);
 
-	int asign = a[0] == '-' ? 1 : 0;
+    int apoint = point_locate(a);
+    int mpoint = (num - (apoint % num)) + apoint;
 
-	point_a = fill_zero(a, lena, lena + mpoint - apoint + ACCURITY * num );
+    int asign = a[0] == '-' ? 1 : 0;
 
-	c = UnsignedBigNumRoot(point_a, num);
+    point_a = fill_zero(a, lena, lena + mpoint - apoint + ACCURACY * num);
 
-	free(point_a);
+    c = UnsignedBigNumRoot(point_a, num);
 
-	return format_real(c, asign, (mpoint / num) + ACCURITY);
+    free(point_a);
+
+    return format_real(c, asign, (mpoint / num) + ACCURACY);
 }
 
